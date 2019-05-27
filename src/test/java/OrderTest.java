05782047110c41a51198899c720322de5b9e280c
Piotr Popiolek <209437@edu.p.lodz.pl>
2019-05-27 20:27:26 +0200

    add test when order state is wrong

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,6 +1,7 @@
 import edu.iis.mto.time.FakeSystemClock;
 import edu.iis.mto.time.Order;
 import edu.iis.mto.time.OrderExpiredException;
+import edu.iis.mto.time.OrderStateException;
 import org.joda.time.DateTime;
 import org.junit.Assert;
 import org.junit.Before;
@@ -14,31 +15,37 @@ public class OrderTest {
     private FakeSystemClock fakeSystemClock;
 
     @Before
-    public void init(){
+    public void init() {
         fakeSystemClock = new FakeSystemClock();
         order = new Order(fakeSystemClock);
 
-        }
+    }
 
 
     @Test(expected = OrderExpiredException.class)
-    public void OrderTestWhenOrderIsCanceled(){
-        fakeSystemClock.setTime(new DateTime(2019,5,27,10,0));
+    public void OrderTestWhenOrderIsCanceled() {
+        fakeSystemClock.setTime(new DateTime(2019, 5, 27, 10, 0));
         order.submit();
-        fakeSystemClock.setTime(new DateTime(2019,5,28,11,0));
+        fakeSystemClock.setTime(new DateTime(2019, 5, 28, 11, 0));
         order.confirm();
     }
-        @Test()
-    public void OrderTestWhenOrderIsSubmitted(){
-        fakeSystemClock.setTime(new DateTime(2019,5,27,10,0));
-        order.submit();
-            Assert.assertThat(order.getOrderState(), is(Order.State.SUBMITTED));
-        }
 
-        @Test()
-    public void OrderTestWhenOrderIsCreated(){
-            Assert.assertThat(order.getOrderState(), is(Order.State.CREATED));
-        }
+    @Test
+    public void OrderTestWhenOrderIsSubmitted() {
+        fakeSystemClock.setTime(new DateTime(2019, 5, 27, 10, 0));
+        order.submit();
+        Assert.assertThat(order.getOrderState(), is(Order.State.SUBMITTED));
+    }
+
+    @Test
+    public void OrderTestWhenOrderIsCreated() {
+        Assert.assertThat(order.getOrderState(), is(Order.State.CREATED));
+    }
+
+    @Test(expected = OrderStateException.class)
+    public void OrderTestWhenOrderStateIsWrong() {
+        order.confirm();
+    }
 
 
 }
